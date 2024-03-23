27aaba22a2c25c3f19923d96c344d76c741ba760
Tristan New <tristannew@ymail.com>
2024-03-23 14:23:50 +0000

    get surf spots ids map fro mgcs

--- good_waves_please/api/spot_ids.py
+++ good_waves_please/api/spot_ids.py
@@ -1,4 +1,4 @@
 import pandas as pd
 from good_waves_please.api.paths import GCS_SURF_SESSIONS_BUCKET
 
-SPOT_IDS_MAP_DF = pd.read_csv(GCS_SURF_SESSIONS_BUCKET / "spot_name_ids_map.csv")
+SPOT_IDS_MAP_DF = pd.read_csv(GCS_SURF_SESSIONS_BUCKET + "spot_name_ids_map.csv")
