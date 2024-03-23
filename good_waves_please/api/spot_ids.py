595a2dd951589009d4bb10e31cdb12ebb675d7ce
Tristan New <tristannew@ymail.com>
2024-03-23 14:36:59 +0000

    get surf spots ids map from gcs

--- good_waves_please/api/spot_ids.py
+++ good_waves_please/api/spot_ids.py
@@ -1,4 +1,7 @@
 import pandas as pd
 from good_waves_please.api.paths import GCS_SURF_SESSIONS_BUCKET
+from st_files_connection import FilesConnection
+import streamlit as st
 
-SPOT_IDS_MAP_DF = pd.read_csv(GCS_SURF_SESSIONS_BUCKET + "spot_name_ids_map.csv")
+conn = st.connection("gcs", type=FilesConnection)
+SPOT_IDS_MAP_DF = conn.read(GCS_SURF_SESSIONS_BUCKET + "spot_name_ids_map.csv", input_format="csv")
