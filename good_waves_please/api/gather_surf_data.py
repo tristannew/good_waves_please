1837c5f83e63a928272eff174940ab6198a89d7b
Tristan New <tristannew@ymail.com>
2024-03-23 15:08:35 +0000

    rewrite rather than append data

--- good_waves_please/api/gather_surf_data.py
+++ good_waves_please/api/gather_surf_data.py
@@ -135,13 +135,21 @@ def write_data_gcs(row: pd.DataFrame):
     conn = st.connection("gcs", type=FilesConnection)
     update = row.copy()
     update.reset_index(drop=True, inplace=True)
-    with conn.open(GCS_SURF_SESSIONS_BUCKET + "database.csv", "a") as file:
-        update.to_csv(
+    current_data = conn.read(GCS_SURF_SESSIONS_BUCKET + "database.csv", input_format='csv')
+    updated_data = pd.concat([current_data, update], ignore_index=True)
+    with conn.open(GCS_SURF_SESSIONS_BUCKET + "database.csv", "w") as file:
+        updated_data.to_csv(
         file,
-        mode="a",
         index=False,
-        header=False,
         )
+    # TODO: it would be more efficient to append the data.
+    # with conn.open(GCS_SURF_SESSIONS_BUCKET + "database.csv", "a") as file:
+    #     update.to_csv(
+    #     file,
+    #     mode="a",
+    #     index=False,
+    #     header=False,
+    #     )
     logger.info("Appended new row to database!")
     return None
 
